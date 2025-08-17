1c0679dda1d91a28d561eaf1b7e38af4d8433445
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2025-08-18 00:01:20 +0800

    check for argc < 2 first in arg_is_invalid

--- src/arg_is_invalid.c
+++ src/arg_is_invalid.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 15:53:36 by kchiang           #+#    #+#             */
-/*   Updated: 2025/08/13 01:03:30 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/08/17 23:59:45 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ int	arg_is_invalid(int argc, char *argv, t_var *var)
 {
 	int	i;
 
+	if (argc != 2)
+		return (true);
 	var->clue_len = ft_strlen(argv);
 	var->row_size = (var->clue_len + 1) / 8;
 	var->ans_size = var->row_size * var->row_size;
-	if (argc != 2 || (var->clue_len + 1) % 4)
+	if ((var->clue_len + 1) % 4)
 		return (true);
 	i = 0;
 	while (i < var->clue_len)
