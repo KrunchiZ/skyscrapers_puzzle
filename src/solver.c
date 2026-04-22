8945bba25f9106442da57a98a10df05bba9da1e2
Kai Zhi Chiang <kchiang@u91z01s06.42kl.edu.my>
2026-04-22 13:18:32 +0800

    comment out pre_solver and full row/col length unique check in src source files

--- src/solver.c
+++ src/solver.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 19:50:11 by kchiang           #+#    #+#             */
-/*   Updated: 2026/04/22 03:50:29 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/04/22 13:12:09 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <stdbool.h>
 
 static int	is_unique(t_var var, int *answer);
-
+/*
 void	pre_solve(int *answer, t_var *var)
 {
 	int	i;
@@ -31,11 +31,11 @@ void	pre_solve(int *answer, t_var *var)
 		if (var->clue[3][i] == 1)
 			answer[i * var->row_size + var->row_size - 1] = var->row_size;
 	}
-}
+}*/
 
 int	solve_puzzle(int *answer, t_var var)
 {
-	pre_solve(answer, &var);
+//	pre_solve(answer, &var);
 	var.n = 1;
 	while (var.n <= var.row_size + 1 && var.pos < var.ans_size)
 	{
@@ -61,14 +61,16 @@ static int	is_unique(t_var var, int *answer)
 {
 	int	i;
 
-	i = var.pos / var.row_size * var.row_size + var.row_size - 1;
+	i = var.pos;
+//	i = var.pos / var.row_size * var.row_size + var.row_size - 1;
 	while (i >= (var.pos / var.row_size * var.row_size))
 	{
 		if (var.n == answer[i])
 			return (false);
 		i--;
 	}
-	i = var.ans_size - var.row_size + var.pos % var.row_size;
+	i = var.pos;
+//	i = var.ans_size - var.row_size + var.pos % var.row_size;
 	while (i >= 0)
 	{
 		if (var.n == answer[i])
